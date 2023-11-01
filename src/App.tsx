8e0d58cffeef5fb259c9aa886ae4a0e1f4a668a4
365saiom <saiom365@naver.com>
2023-11-01 19:04:47 +0900

    MouseEvent

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
-    </div>
-  );
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+interface DummyProps {
+  text: string;
+  active?: boolean;
 }
 
+function Dummy({ text, active = false }: DummyProps) {
+  return <h1>{text}</h1>;
+}
+
+function App() {
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {};
+  return (
+    <Container>
+      <Dummy text="hello" />
+      <button onClick={onClick}>click me</button>
+    </Container>
+  );
+}
 export default App;
