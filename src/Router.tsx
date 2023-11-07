9704705c61c9d8381c2a68c26aac0de089f73ee7
365saiom <saiom365@naver.com>
2023-11-07 18:07:05 +0900

    CRYPTO TRACKER

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,39 +1,16 @@
-import { createBrowserRouter } from "react-router-dom";
-import About from "./screens/About";
-import Home from "./screens/Home";
-import Root from "./Root";
-import NotFound from "./screens/NotFound";
-import ErrorComponent from "./components/ErrorComponent";
-import User from "./screens/users/User";
-import Followers from "./screens/users/Followers";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Coin from "./routes/Coin";
+import Coins from "./routes/Coins";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-        errorElement: <ErrorComponent />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "users/:userId",
-        element: <User />,
-        children: [
-          {
-            path: "followers",
-            element: <Followers />,
-          },
-        ],
-      },
-    ],
-    errorElement: <NotFound />,
-  },
-]);
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/:coinId" element={<Coin />}></Route>
+        <Route path="/" element={<Coins />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-export default router;
+export default Router;
