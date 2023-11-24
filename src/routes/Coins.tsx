6de03ae0f6dd40c4777b237b94204d792d780ff7
365saiom <365saiom@gmail.com>
2023-11-25 01:25:47 +0900

    Coin 데이터 일부를 dummy 데이터로 불러와서 map()함수 구현

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,14 +1,39 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  padding: 0px 20px;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const CoinsList = styled.ul``;
 
-const Coin = styled.li``;
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  padding: 20px;
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    transition: color 0.2s ease-in; // color의 색상이 서서히 바뀌는 효과
+    display: block;
+    padding: 20px;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
 
 const Title = styled.h1`
+  font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
 
@@ -43,17 +68,19 @@ const coins = [
 ];
 
 function Coins() {
-  return;
-  <Container>
-    <Header>
-      <Title>코인</Title>;
-    </Header>
-    <CoinsList>
-      {coins.map((coin) => (
-        <Coin key={coin.id}>{coin.name}</Coin>
-      ))}
-    </CoinsList>
-  </Container>;
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      <CoinsList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+          </Coin>
+        ))}
+      </CoinsList>
+    </Container>
+  );
 }
-
 export default Coins;
--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,5 @@ import { DefaultTheme } from "styled-components";
 export const theme: DefaultTheme = {
   bgColor: "#2f3640",
   textColor: "#fbc531",
-  accentColor: "#f5f6fa",
+  accentColor: "#8c7ae6",
 };
